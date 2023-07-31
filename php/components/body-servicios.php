6ee8c502ee2ed9ca04bacbb06c860d6e521469cc
LuisSalgadoAlz <113075038+LuisSalgadoAlz@users.noreply.github.com>
2023-07-30 18:11:34 -0600

    Modals servicios y paquetes

--- php/components/body-servicios.php
+++ php/components/body-servicios.php
@@ -11,9 +11,9 @@
                         <span class="">Gestion de paquete</span>
                     </div>
                     <div class="col-4 d-flex justify-content-end px-3">
-                        <button class="btn btn-secondary btn-sm">
-                            <i class='bx bx-message-square-add me-2'></i>
-                            Nuevo paquetes
+                        <button class="btn d-flex gap-2 align-items-center" data-bs-toggle="modal" data-bs-target="#paquetesModal">
+                            <i class='bx bx-message-square-add'></i>
+                            <span class="subtitle-contratos">Nuevo paquete</span>
                         </button>
                     </div>
                 </div>
@@ -51,9 +51,9 @@
                         <span class="">Gestion de paquete</span>
                     </div>
                     <div class="col-4 d-flex justify-content-end px-3">
-                        <button class="btn btn-secondary btn-sm">
-                            <i class='bx bx-message-square-add me-2'></i>
-                            Nuevo servicio
+                        <button class="btn d-flex gap-2 align-items-center" data-bs-toggle="modal" data-bs-target="#serviciosModal">
+                            <i class='bx bx-message-square-add'></i>
+                            <span class="subtitle-contratos">Nuevo servicio</span>
                         </button>
                     </div>
                 </div>
@@ -187,6 +187,69 @@
                         </div>
                     </div>
                 </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="paquetesModal" tabindex="-1" aria-labelledby="paquetesModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Creacion - Nuevo paquete</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Nombre del paquete</label>
+                    <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Eje. paquete basico">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Servicio inicial</label>
+                    <select class="form-select form-select-sm" aria-label=".form-select-sm example">
+                        <option selected>Selecione una opcion</option>
+                        <option value="1">One</option>
+                        <option value="2">Two</option>
+                        <option value="3">Three</option>
+                    </select>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Precio inicial</label>
+                    <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Precio del servicio inicial" disabled>
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary">Finalizar</button>
+            </div>
+        </div>
+    </div>
+</div>
+
+<div class="modal fade" id="serviciosModal" tabindex="-1" aria-labelledby="serviciosModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="exampleModalLabel">Creacion - Nuevo tecnico</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Nombre</label>
+                    <input type="text" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="Perez">
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlTextarea1" class="form-label">Descripcion</label>
+                    <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
+                </div>
+                <div class="mb-3">
+                    <label for="exampleFormControlInput1" class="form-label">Precio</label>
+                    <input type="number" class="form-control form-control-sm" id="exampleFormControlInput1" placeholder="1200 Lps">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Cancelar</button>
+                <button type="button" class="btn btn-primary">Finalizar</button>
             </div>
         </div>
     </div>
